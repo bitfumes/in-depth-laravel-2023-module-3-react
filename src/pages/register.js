b341c1b6f6408edb0c247db18e31085e1ae8c149
sarthaksavvy <sarthak@bitfumes.com>
2023-04-17 13:45:51 +0530

    [main] 46 - React register form handle

---
+++ src/pages/register.js
@@ -0,0 +1,79 @@
+import { Button, Card, Input, Typography } from "@material-tailwind/react";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Register() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(form);
+  }
+
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <Card color="transparent" shadow={false}>
+          <Typography variant="h4" color="blue-gray">
+            Sign Up
+          </Typography>
+          <Typography color="gray" className="mt-1 font-normal">
+            Enter your details to register.
+          </Typography>
+          <form
+            onSubmit={handleSubmit}
+            className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+          >
+            <div className="mb-4 flex flex-col gap-6">
+              <Input
+                value={form.name}
+                onChange={handleChange}
+                size="lg"
+                label="Name"
+                name="name"
+              />
+              <Input
+                value={form.email}
+                onChange={handleChange}
+                size="lg"
+                label="Email"
+                name="email"
+              />
+              <Input
+                value={form.password}
+                onChange={handleChange}
+                type="password"
+                size="lg"
+                label="Password"
+                name="password"
+              />
+            </div>
+            <Button type="submit" className="mt-6" fullWidth>
+              Register
+            </Button>
+            <Typography color="gray" className="mt-4 text-center font-normal">
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                className="font-medium text-blue-500 transition-colors hover:text-blue-700"
+              >
+                Log In
+              </Link>
+            </Typography>
+          </form>
+        </Card>
+      </div>
+    </div>
+  );
+}
