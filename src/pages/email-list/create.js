7ed17dfc212f9c5db1ab13d0e4f03a2ce9a3e2d5
sarthaksavvy <sarthak@bitfumes.com>
2023-04-19 13:46:14 +0530

    [main] 59 - React Nextjs email list create

--- src/pages/email-list/create.js
+++ src/pages/email-list/create.js
@@ -1,3 +1,87 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Input,
+  Typography,
+} from "@material-tailwind/react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 export default function Create() {
-  return <div>create</div>;
+  const router = useRouter();
+  const [errors, setErrors] = useState({});
+  const [form, setForm] = useState({
+    email: "",
+    description: "",
+  });
+
+  async function submit() {
+    const token = Cookies.get("token");
+    const res = await fetch("http://localhost:8000/api/email-list", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      toast.success("Email list created successfully!");
+      router.push("/email-list");
+      return;
+    }
+
+    toast.error("Check for any error!");
+    if (data.errors) {
+      setErrors(data.errors);
+    }
+  }
+
+  return (
+    <div className="flex justify-center w-full">
+      <Card className="w-1/2">
+        <CardHeader
+          variant="gradient"
+          color="blue"
+          className="mb-4 grid h-28 place-items-center"
+        >
+          <Typography variant="h3" color="white">
+            Create Email List
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <Input
+            value={form.name}
+            onChange={(e) =>
+              setForm({ ...form, [e.target.name]: e.target.value })
+            }
+            name="name"
+            label="Name"
+            size="lg"
+          />
+          <Input
+            value={form.description}
+            onChange={(e) =>
+              setForm({ ...form, [e.target.name]: e.target.value })
+            }
+            name="description"
+            label="Description"
+            size="lg"
+          />
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button onClick={submit} variant="gradient" fullWidth>
+            Create
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
--- src/pages/email-list/index.js
+++ src/pages/email-list/index.js
@@ -1,7 +1,34 @@
 import { Button, Typography } from "@material-tailwind/react";
+import Cookies from "js-cookie";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Emailist() {
+  const [lists, setLists] = useState([]);
+
+  async function getLists() {
+    const token = Cookies.get("token");
+    const res = await fetch("http://localhost:8000/api/email-list", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    const { data } = await res.json();
+    if (res.ok) {
+      setLists(data);
+      return;
+    }
+
+    toast.error("Check for any error!");
+  }
+
+  useEffect(() => {
+    getLists();
+  }, []);
+
   return (
     <div>
       <div className="flex justify-between">
@@ -21,53 +48,28 @@ export default function Emailist() {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                Name
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
+                Description
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            {lists.map((list) => (
+              <tr
+                key={list.id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
               >
-                Apple MacBook Pro 17
-              </th>
-              <td className="px-6 py-4">Silver</td>
-              <td className="px-6 py-4">Laptop</td>
-              <td className="px-6 py-4">$2999</td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-            </tr>
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {list.name}
+                </th>
+                <td className="px-6 py-4">{list.description}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
